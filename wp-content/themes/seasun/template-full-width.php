60b0fa7f8876fb06ecdfedd27f61f58528df28c1
Sarah <shlcapecci@gmail.com>
2014-11-04 09:43:21 -0500

    setup the environment

---
+++ wp-content/themes/seasun/template-full-width.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: Full Width
+ * The template file for pages without right sidebar.
+ * @package SeaSun
+ * @since SeaSun 1.0.0
+*/
+get_header(); ?>
+<div id="wrapper-content">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <div class="content-headline-wrapper">
+    <div class="content-headline">
+      <h1><?php the_title(); ?></h1>
+<?php seasun_get_breadcrumb(); ?>
+    </div>
+  </div>
+  <div class="container">
+  <div id="main-content">
+    <div id="content">
+<?php seasun_get_display_image_page(); ?> 
+      <div class="entry-content">
+<?php the_content(); ?>
+<?php edit_post_link( __( '(Edit)', 'seasun' ), '<p>', '</p>' ); ?>
+      </div>
+<?php endwhile; endif; ?>
+<?php comments_template( '', true ); ?>
+    </div> <!-- end of content -->
+  </div>
+  </div>
+</div>     <!-- end of wrapper-content -->
+<?php get_footer(); ?>
