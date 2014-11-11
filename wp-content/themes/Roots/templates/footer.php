aaf200e03b952e870162580ff78faa0a206fbee6
Sarah <shlcapecci@gmail.com>
2014-11-11 11:05:32 -0500

    footer styled

--- wp-content/themes/Roots/templates/footer.php
+++ wp-content/themes/Roots/templates/footer.php
@@ -1,11 +1,11 @@
-<footer role="contentinfo">
-	<div class="container">
-		<ul>
-			<li><img src="<?php echo get_template_directory_uri(); ?>/assets/img/youth_logo_notext.png"></li>
-			<li><img src="<?php echo get_template_directory_uri(); ?>/assets/img/ontario_logo.png"></li>
-			<li><img src="<?php echo get_template_directory_uri(); ?>/assets/img/access_logo.png"></li>
-		</ul>
+<footer role="contentinfo">	
+	<ul>
+		<li class="text-al-right"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/youth_logo_notext.png"></li>
+		<li class="text-al-center"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/ontario_logo.png"></li>
+		<li><img src="<?php echo get_template_directory_uri(); ?>/assets/img/access_logo.png"></li>
+	</ul>
+	<div class="blue-bg">
 		<img src="<?php echo get_template_directory_uri(); ?>/assets/img/torch_logo.png" alt="">
-		<p>&copy;  Regional Youth Roundtable</p>
+		<p class="white-txt text-al-center font-thin">&copy;  Regional Youth Roundtable</p>
 	</div>
 </footer>
