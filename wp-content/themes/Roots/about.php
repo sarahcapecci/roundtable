0f145dac8a84b34e0d0a6db5793f70d152e27960
Sarah <shlcapecci@gmail.com>
2014-11-11 14:03:16 -0500

    about page styled

--- wp-content/themes/Roots/about.php
+++ wp-content/themes/Roots/about.php
@@ -19,25 +19,39 @@ This is the page where the About content is displayed
 	    $page_bg_image_url = get_background_image();
 	}
 ?>
-<div class="about-header bg-set" style="background-image: url(<?php echo $page_bg_image_url ?>);">
+<div class="content-wrapper">
+	<div class="about-header bg-set" style="background-image: url(<?php echo $page_bg_image_url ?>);">
+	</div>
+	<p class="about-text text-al-center font-lg">The <span class="blue-txt">Regional Youth Roundtable</span> is a platform for sharing ideas and initiatives between youth-led organizations in Peel, while alleviating pressures facing minority youth.</p>
+	<div class="vertical-al-top about-text inline-block">
+	<?php while (have_posts()) : the_post(); ?>
+	  <?php echo the_content(); ?>
+	<?php endwhile; ?>
+	</div>
+	<?php $small_image = get_field('small_image'); ?>
+	<div class="vertical-al-top bg-set about-thumbnail inline-block" style="background-image: url(<?php echo $small_image['url'] ?>);"></div>
+	<section class="values">
+		<h3 class="orange-txt text-al-center">Our Mission</h3>
+		<?php echo get_field('mission'); ?>
+	</section>
+	<section class="values">
+		<h3 class="orange-txt text-al-center">Our Vision</h3>
+		<?php echo get_field('vision'); ?>
+	</section>
+	<div class="sponsors">
+	<h3 class="blue-txt">Our Sponsors &amp; Funders</h3>
+		<?php while( has_sub_field('sponsors') ): ?>
+			<?php $sponsor_img = get_sub_field('thumbnail'); ?>
+		    <span class="thumbnail bg-set" style="background-image: url(<?php echo $sponsor_img['url'] ?>);">
+		    </span>
+		<?php endwhile; ?>
+	</div>
+	<div class="sponsors">
+		<h3 class="blue-txt">Our Partners &amp; Friends</h3>
+		<?php while( has_sub_field('partners') ): ?>
+			<?php $partner_img = get_sub_field('thumbnail'); ?>
+		    <span class="thumbnail bg-set" style="background-image: url(<?php echo $partner_img['url'] ?>);">
+		    </span>
+		<?php endwhile; ?>
+	</div>
 </div>
-<?php $small_image = get_field('small_image'); ?>
-<div class="whatever-bigger" style="background-image: url(<?php echo $small_image['url'] ?>);"></div>
-<h3>MISSION</h3>
-<?php echo get_field('mission'); ?>
-<h3>VISION</h3>
-<?php echo get_field('vision'); ?>
-
-<h3>Sponsors</h3>
-<?php while( has_sub_field('sponsors') ): ?>
-	<?php $sponsor_img = get_sub_field('thumbnail'); ?>
-    <span class="whatever" style="background-image: url(<?php echo $sponsor_img['url'] ?>);">
-    </span>
-<?php endwhile; ?>
-
-<h3>Partners and Friends</h3>
-<?php while( has_sub_field('partners') ): ?>
-	<?php $partner_img = get_sub_field('thumbnail'); ?>
-    <span class="whatever" style="background-image: url(<?php echo $partner_img['url'] ?>);">
-    </span>
-<?php endwhile; ?>
