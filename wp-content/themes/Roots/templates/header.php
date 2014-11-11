9540b36f662702a83f9e5ab0b5f67cb003625112
Sarah <shlcapecci@gmail.com>
2014-11-11 10:32:03 -0500

    header styled

--- wp-content/themes/Roots/templates/header.php
+++ wp-content/themes/Roots/templates/header.php
@@ -1,14 +1,11 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/youth_logo.png" alt=""></a>
-    </div>
-    <nav class="collapse navbar-collapse" role="navigation">
+<header class="header" role="banner">
+    <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/youth_logo.png" alt=""></a>
+    <nav role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'main-navbar'));
         endif;
       ?>
     </nav>
-  </div>
+    <img class="transparency" src="<?php echo get_template_directory_uri(); ?>/assets/img/top_transparency.png" alt="">
 </header>
