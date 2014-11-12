88327a2a80701c2a52159a8cfabcd4a16a41d0c7
Sarah <shlcapecci@gmail.com>
2014-11-12 11:17:01 -0500

    connect popup created

---
+++ wp-content/themes/Roots/templates/connect_popup.php
@@ -0,0 +1,25 @@
+<?php 
+// Connect popup
+?>
+
+<div class="blue-bg">
+	<div class="left inline-block vertical-al-top">
+		<h3>Connecting with RYR</h3>
+		<p>Fill out the form to the right and the appropriate Regional Youth Roundtable team member will reply via email.</p>
+		<h3>Let's Get Social</h3>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	<div class="right inline-block vertical-al-top">
+		[contact-form-7 id="86" title="Connect"]
+	</div>
+	
+</div>
