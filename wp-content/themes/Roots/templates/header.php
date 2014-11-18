fb8a7cb9d037d3be8e95c2593104169c158b5563
Sarah <shlcapecci@gmail.com>
2014-11-18 11:44:26 -0500

    navigation adapted for mobile

--- wp-content/themes/Roots/templates/header.php
+++ wp-content/themes/Roots/templates/header.php
@@ -1,12 +1,24 @@
 <header class="header" role="banner">
     <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/youth_logo.png" alt=""></a>
-    <nav role="navigation">
+    <nav class="tablet-hide" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'main-navbar'));
         endif;
       ?>
-      <button class="connect" data-toggle="modal" data-target="#myModal" type="button">Connect</button>
+      <button class="connect" data-toggle="modal" data-target="#myModal" type="button">connect</button>
     </nav>
-    <img class="transparency" src="<?php echo get_template_directory_uri(); ?>/assets/img/top_transparency.png" alt="">
+    <nav class="tablet-show" role="navigation">
+      <ul class="mobile-nav">
+        <li class="active menu-updates"><a href="http://localhost:8888/roundtable/">updates</a></li>
+        <li id="dropdown" class="dropdown"><i class="fa fa-bars font-lg gray-txt pointer"></i>
+          <ul class="mobile-dropdown">
+            <li class="menu-about"><a href="http://localhost:8888/roundtable/about/">about</a></li>
+            <li class="menu-team"><a href="http://localhost:8888/roundtable/team/">team</a></li>
+            <li class="menu-members"><a href="http://localhost:8888/roundtable/members/">members</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <img class="tablet-hide transparency" src="<?php echo get_template_directory_uri(); ?>/assets/img/top_transparency.png" alt="">
 </header>
