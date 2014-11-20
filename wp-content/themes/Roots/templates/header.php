ed991d8ec57ebb6d8697689de1f51ceefabe7e6d
Sarah <shlcapecci@gmail.com>
2014-11-20 11:50:02 -0500

    updated the nav bar for mobile

--- wp-content/themes/Roots/templates/header.php
+++ wp-content/themes/Roots/templates/header.php
@@ -10,13 +10,14 @@
     </nav>
     <nav class="tablet-show" role="navigation">
       <ul class="mobile-nav">
-        <li class="active menu-updates"><a href="http://localhost:8888/roundtable/">updates</a></li>
+        <li id="current-page" class="active"></li>
         <li class="dropdown"><a id="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#"><i class="fa fa-bars font-lg gray-txt pointer"></a></i>
           <ul id="mobile-dropdown" class="mobile-dropdown dropdown-menu">
+            <li class="menu-about"><a href="http://localhost:8888/roundtable/">updates</a></li>
             <li class="menu-about"><a href="http://localhost:8888/roundtable/about/">about</a></li>
             <li class="menu-team"><a href="http://localhost:8888/roundtable/team/">team</a></li>
             <li class="menu-members"><a href="http://localhost:8888/roundtable/members/">members</a></li>
-            <li><button data-toggle="modal" data-target="#myModal">connect</button></li>
+            <li><button class="transparent-bg" data-toggle="modal" data-target="#myModal">connect</button></li>
           </ul>
         </li>
       </ul>
